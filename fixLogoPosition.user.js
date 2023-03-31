9e6774ba492cd07aa79f799469b87411aef1f59b
AcesFullOfKings <6105690+theonefoster@users.noreply.github.com>
2023-03-31 22:26:55 +0100

    Update fixLogoPosition.user.js

--- fixLogoPosition.user.js
+++ fixLogoPosition.user.js
@@ -2,7 +2,7 @@
 // @name          SB Browser navbar logo position
 // @namespace     https://sb.ltn.fi
 // @description   Adjusts the position of the logo image in the SB Browser navbar on sb.ltn.fi
-// @version       1
+// @version       1.0.1
 // @grant         none
 // @author        ChatGPT
 // @match         https://sb.ltn.fi/*
