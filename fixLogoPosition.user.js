d43421bc35d4e85ea7d08977d31c78e39c069814
AcesFullOfKings <6105690+theonefoster@users.noreply.github.com>
2023-03-31 23:36:16 +0100

    Add Icon

--- fixLogoPosition.user.js
+++ fixLogoPosition.user.js
@@ -2,12 +2,13 @@
 // @name          SB Browser navbar logo position
 // @namespace     https://sb.ltn.fi
 // @description   Adjusts the position of the logo image in the SB Browser navbar on sb.ltn.fi
-// @version       1.0.1
+// @version       1.0.2
 // @grant         none
 // @author        ChatGPT
 // @match         https://sb.ltn.fi/*
 // @updateURL     https://github.com/theonefoster/SponsorBlock-UserScripts/raw/main/fixLogoPosition.user.js
 // @downloadURL   https://github.com/theonefoster/SponsorBlock-UserScripts/raw/main/fixLogoPosition.user.js
+// @icon          https://sb.ltn.fi/static/browser/logo.png
 // ==/UserScript==
 
 (function() {
